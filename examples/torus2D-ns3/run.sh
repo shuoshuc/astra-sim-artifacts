47da42a51711b8cb017bbf0e373e8cf3e5bbb60f
shawn <shuoshuc@cs.cmu.edu>
2025-08-31 18:00:32 -0400

    Add 2D examples with communicator group.

---
+++ examples/torus2D-ns3/run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+# Path
+SCRIPT_DIR=$(dirname "$(realpath $0)")
+# examples folder should be parallel to astra-sim and other folders.
+# BASE_DIR is the /app folder in the provided docker container.
+BASE_DIR=${SCRIPT_DIR}/../../
+ASTRA_SIM_BUILD_DIR=${BASE_DIR}/astra-sim/extern/network_backend/ns-3/build/scratch/
+ASTRA_SIM=./ns3.42-AstraSimNetwork-default
+
+# Run ASTRA-sim
+(
+cd ${ASTRA_SIM_BUILD_DIR}
+touch ../../scratch/output/flow.txt
+${ASTRA_SIM} \
+    --workload-configuration=${SCRIPT_DIR}/trace/trace \
+    --system-configuration=${SCRIPT_DIR}/inputs/Ring_Ring_sys.json \
+    --remote-memory-configuration=${SCRIPT_DIR}/inputs/RemoteMemory.json \
+    --logical-topology-configuration=${SCRIPT_DIR}/inputs/logical_network.json \
+    --network-configuration=${SCRIPT_DIR}/inputs/ns3_config.txt \
+    --comm-group-configuration=${SCRIPT_DIR}/inputs/comm_group.json
+)
