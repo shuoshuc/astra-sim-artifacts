ce9d284672df6ba4a876006d905ddb76366467cb
shawn <shuoshuc@cs.cmu.edu>
2025-11-13 21:02:53 -0500

    Switch to reconfigurable backend.

--- README.md
+++ README.md
@@ -22,9 +22,17 @@ cd examples/T1D-ns3
 # Tools
 The tools/ folder contains useful tools to help with the experiments.
 
+## Build torus
 For example, build\_torus.py can construct a physical torus network given specified parameters.
 ```bash
 python3 tools/build_torus.py --dims 4 4 -b 400Gbps -l 0.001ms -o physical_16nodes_2D.txt
+```
+
+## Generate STG traces
+To generate traces using STG, make sure STG is installed in the docker container (e.g., at /app/STG).
+Running the following command in the docker container creates a 4-XPU trace called workload.[0-3].et
+```bash
+cd /app/STG && mkdir -p tmp_trace && python3 main.py --output_dir ./tmp_trace --output_name "workload" --model_type "dense" --dp 2 --tp 2 --pp 1 --weight_sharded 0 --chakra_schema_version "v0.0.4"
 ```
 
 ## Merge traces
---
+++ examples/multitenant-reconfig/run.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+# Path
+SCRIPT_DIR=$(dirname "$(realpath $0)")
+# examples folder should be parallel to astra-sim and other folders.
+# BASE_DIR is the /app folder in the provided docker container.
+BASE_DIR=${SCRIPT_DIR}/../../
+STG_DIR=${BASE_DIR}/STG
+ASTRA_SIM=${BASE_DIR}/astra-sim/build/astra_analytical/build/bin/AstraSim_Analytical_Reconfigurable
+TRACE_PATH=${SCRIPT_DIR}/trace/merged/
+
+# TODO: Merge traces for multi-tenant scenarios.
+mkdir -p ${TRACE_PATH}
+cd ${STG_DIR}
+python3 ${STG_DIR}/main.py --output_dir ${TRACE_PATH} --output_name "trace" \
+    --model_type "dense" --dp 2 --tp 2 --pp 1 \
+    --weight_sharded 0 --chakra_schema_version "v0.0.4"
+
+# Run ASTRA-sim
+(
+${ASTRA_SIM} \
+    --workload-configuration=${SCRIPT_DIR}/trace/merged/trace \
+    --comm-group-configuration=${SCRIPT_DIR}/trace/merged/trace.json \
+    --system-configuration=${SCRIPT_DIR}/inputs/sys.json \
+    --network-configuration=${SCRIPT_DIR}/inputs/network.yml \
+    --remote-memory-configuration=${SCRIPT_DIR}/inputs/RemoteMemory.json \
+    --circuit-schedules=${SCRIPT_DIR}/inputs/schedule.txt
+)
