6ce45c56e5169ba49278fbf666ed28ee1f0ea940
shawn <shuoshuc@cs.cmu.edu>
2025-12-27 00:14:45 -0500

    Integrate jobspec into the experiment workflow.

--- examples/multitenant-reconfig/run.sh
+++ examples/multitenant-reconfig/run.sh
@@ -21,7 +21,11 @@ BW=50
 JOB_NAMES=()
 JOB_SHAPES=""
 
-# Generate traces using STG
+# [Step 1] Prepare jobspec with main jobs and background jobs.
+python ${TOOLS_PATH}/create_jobspec.py -D "${TORUS_X_SIZE}x${TORUS_Y_SIZE}x${TORUS_Z_SIZE}" \
+    -J "2x2x2,2x2x1" -o "${INPUT_PATH}/jobspec.txt"
+
+# [Step 2] Generate traces using STG
 cd ${STG_DIR}
 while IFS="," read -r JOB DP TP PP || [[ -n "$JOB" ]]; do
     mkdir -p ${TRACE_PATH}/${JOB}
@@ -37,23 +41,23 @@ while IFS="," read -r JOB DP TP PP || [[ -n "$JOB" ]]; do
     JOB_SHAPES="${JOB_SHAPES}${JOB}:${DP}x${TP}x${PP}"
 done < "${INPUT_PATH}/jobspec.txt"
 
-# Generate placement for multi-tenant scenarios.
+# [Step 3] Generate placement for multi-tenant scenarios.
 python ${TOOLS_PATH}/place.py -D "${TORUS_X_SIZE}x${TORUS_Y_SIZE}x${TORUS_Z_SIZE}" \
     -B ${BLOCK_SIZE} -J ${JOB_SHAPES} -o ${INPUT_PATH}/placement.json
 
-# Merge traces for multi-tenant scenarios.
+# [Step 4] Merge traces for multi-tenant scenarios.
 cd ${SCRIPT_DIR}
 mkdir -p ${TRACE_PATH}/merged
 TRACES=$(IFS=, ; echo "${JOB_NAMES[*]}")
 python ${TOOLS_PATH}/merge_trace.py -i ${TRACE_PATH} --traces ${TRACES} -o ${TRACE_PATH}/merged/ -p ${INPUT_PATH}/placement.json
 
-# Generate BW matrix for torus. Update bandwidth (bw) and npu count in the network config.
+# [Step 5] Generate BW matrix for torus. Update bandwidth (bw) and npu count in the network config.
 NPUS=$((TORUS_X_SIZE * TORUS_Y_SIZE * TORUS_Z_SIZE))
 python ${TOOLS_PATH}/gen_bw_matrix.py -x ${TORUS_X_SIZE} -y ${TORUS_Y_SIZE} -z ${TORUS_Z_SIZE} -bw ${BW} -o ${INPUT_PATH}/schedule.txt
 sed -i "s/npus_count: \[ .* \]/npus_count: [ ${NPUS} ]/" ${INPUT_PATH}/network.yml
 sed -i "s/bandwidth: \[ .* \]/bandwidth: [ ${BW} ]/" ${INPUT_PATH}/network.yml
 
-# Run ASTRA-sim
+# [Step 6] Run ASTRA-sim
 (
 ${ASTRA_SIM} \
     --workload-configuration=${TRACE_PATH}/merged/trace \
@@ -64,5 +68,5 @@ ${ASTRA_SIM} \
     --circuit-schedules=${INPUT_PATH}/schedule.txt
 )
 
-# Extract JCT into a csv file.
+# [Step 7] Extract JCT into a csv file.
 python ${TOOLS_PATH}/extract_jct.py -p ${INPUT_PATH}/placement.json -l ${SCRIPT_DIR}/log/jct.log -o ${SCRIPT_DIR}/jct.csv
