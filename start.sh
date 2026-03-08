01c716255cc2525f4dc7e6b2a67fab3a5ca80fc4
Shawn Chen <shuoshuc@cs.cmu.edu>
2026-03-08 18:56:14 -0400

    Update start script

--- start.sh
+++ start.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
-POLICY=$1
-MAIN_JOBS=$2
+POLICY=${1:-"firstfit"}
+MAIN_JOBS=${2:-"2x2x2"}
+TORUS_X_SIZE=${3:-16}
+TORUS_Y_SIZE=${4:-16}
+TORUS_Z_SIZE=${5:-16}
+BW=${6:-50}
+BG_JOBS=${7:-"1x1x1"}
+DUMMY=${8:-true}
 
 mkdir -p output
 sudo docker run --ipc=host \
     -v $(pwd)/examples:/app/examples \
     -v $(pwd)/tools:/app/tools \
-    astra /bin/bash -c "/app/examples/multitenant-reconfig/run.sh ${POLICY} ${MAIN_JOBS}"
+    astra /bin/bash -c "/app/examples/multitenant-reconfig/run.sh \
+        ${POLICY} \
+        ${MAIN_JOBS} \
+        ${TORUS_X_SIZE} \
+        ${TORUS_Y_SIZE} \
+        ${TORUS_Z_SIZE} \
+        ${BW} \
+        ${BG_JOBS} \
+        ${DUMMY}"
 mv $(pwd)/examples/multitenant-reconfig/jct.csv output
 cd $(pwd)/examples/multitenant-reconfig
 sudo rm -r tmp log trace inputs/jobspec.txt inputs/placement.json inputs/schedule.txt
